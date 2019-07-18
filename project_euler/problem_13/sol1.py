70b991594ead6848ba66b1873c059b39ddaef10a
cclauss <cclauss@me.com>
2019-07-18 22:01:27 +0200

    Travis CI: Add a flake8 test for unused imports

--- project_euler/problem_13/sol1.py
+++ project_euler/problem_13/sol1.py
@@ -3,18 +3,11 @@ Problem Statement:
 Work out the first ten digits of the sum of the following one-hundred 50-digit
 numbers.
 """
-from __future__ import print_function
-import os
-
-try:
-    raw_input  # Python 2
-except NameError:
-    raw_input = input  # Python 3
 
 
 def solution(array):
     """Returns the first ten digits of the sum of the array elements.
-    
+
     >>> sum = 0
     >>> array = []
     >>> with open(os.path.dirname(__file__) + "/num.txt","r") as f:
